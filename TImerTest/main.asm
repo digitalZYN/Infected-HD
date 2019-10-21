794959ffa1e7835c95e3dfec5f63c3ba69ce6c5d
Zyn <digital.zyn@gmail.com>
2019-10-20 18:11:20 -0700

    IR
    IR SEND 38khz

--- TImerTest/main.asm
+++ TImerTest/main.asm
@@ -1,9 +1,3 @@
-;
-; TImerTest.asm
-;
-; Created: 10/18/2019 6:06:54 PM
-; Author : Zyn
-;
 .def TEMP = R16
 
 .equ TB0_CTRLA = 0xA40
@@ -29,36 +23,45 @@
 .equ PRTA_DIR = 0x400
 
 .org 0x0000
-	rjmp	RESET
+    rjmp    RESET
 
 ; Replace with your application code
 RESET:
 
-	; Enable output of TCB
-	ldi		TEMP, (1 << TCB0)
-	sts		PORTMX_CTRLD, TEMP
+    ; Enable output of TCB
+    ldi        TEMP, (1 << TCB0)
+    sts        PORTMX_CTRLD, TEMP
 
-	ldi		TEMP, (1 << 6)
-	sts		PRTA_DIR, TEMP
+    ldi        TEMP, (1 << 6)
+    sts        PRTA_DIR, TEMP
 
-	; Configure Timer B
-	ldi		TEMP, (0 << TB0_CLKSEL1) | (0 << TB0_CLKSEL0) | (1 << TB0_ENABLE)
-	sts		TB0_CTRLA, TEMP
-	
-	ldi		TEMP, (1 << TB0_CNTMODE2) | (1 << TB0_CNTMODE1) | (1 << TB0_CNTMODE0) | (0 << TB0_CCMPEN)
-	sts		TB0_CTRLB, TEMP
+    ; Allow Event To Pass-Through to the pin
+    ldi        TEMP, 0x01
+    sts        0x18A, TEMP
+    sts        0x192, TEMP
 
-	; Set to count 26 times for 38kHz
-	ldi		TEMP, 0x1A
-	sts		TB0_CCMPL, TEMP
 
-	ldi		TEMP, 0x04
-	sts		TB0_CCMPH, TEMP
+    ;ldi        TEMP, 0x10
+    ;sts        0x182, TEMP
 
-	; Enable all interrupts
-	sei
+    ; Configure Timer B
+    ldi        TEMP, (0 << TB0_CLKSEL1) | (0 << TB0_CLKSEL0) | (1 << TB0_ENABLE)
+    sts        TB0_CTRLA, TEMP
+    
+    ldi        TEMP, (1 << TB0_CNTMODE2) | (1 << TB0_CNTMODE1) | (1 << TB0_CNTMODE0) | (1 << TB0_CCMPEN)
+    sts        TB0_CTRLB, TEMP
+
+    ; Set to count 26 times for 38kHz
+    ldi        TEMP, 0x1A
+    sts        TB0_CCMPL, TEMP
+
+    ldi        TEMP, 0x04
+    sts        TB0_CCMPH, TEMP
+
+    ; Enable all interrupts
+    sei
 
 MAIN:
 
-	nop
-	rjmp	MAIN
+    nop
+    rjmp    MAIN
